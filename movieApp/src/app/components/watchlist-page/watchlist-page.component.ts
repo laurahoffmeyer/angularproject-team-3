35d6e92b80a96858627526a44c344084f3ab0afb
lhoffmeyer89 <lhoffmeyer89@gmail.com>
2021-01-29 00:24:34 -0500

    pushing to main

--- movieApp/src/app/components/watchlist-page/watchlist-page.component.ts
+++ movieApp/src/app/components/watchlist-page/watchlist-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MovieService } from 'src/app/services/movie.service';
 import { Movie } from '../../interfaces/movie';
 
@@ -9,13 +9,26 @@ import { Movie } from '../../interfaces/movie';
 })
 export class WatchlistPageComponent implements OnInit {
 
+  @Input() wLMovies: Movie = {
+    title: "",
+    id: 0,
+    overview: "",
+    release_date: "",
+    vote_average: 0,
+    genre_ids: [],
+    backdrop_path: "",
+    poster_path: "",
+    watchList: false
+  }
+  @Input() index: any;
+  
   constructor(private service: MovieService) { }
 
   ngOnInit(): void {
   }
 
-  get watchList(): Movie[] {
-    return this.service.movies;
+  removeFromWatchlist(index: any) {
+    this.service.watchListArray.splice(index, 1);
   }
 
 }
