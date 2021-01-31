87b6520175f5a9d0226752c6d1abd09f7335a530
Nicole <nbush89@gmail.com>
2021-01-31 15:34:36 -0500

    added genre names nd movie details

--- movieApp/src/app/components/movie-list/movie-list.component.ts
+++ movieApp/src/app/components/movie-list/movie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Genre } from 'src/app/interfaces/genre';
 import { MovieService } from 'src/app/services/movie.service';
 import { Movie } from '../../interfaces/movie';
 import { SearchCriteriaComponent } from '../search-criteria/search-criteria.component';
@@ -6,29 +7,51 @@ import { SearchCriteriaComponent } from '../search-criteria/search-criteria.comp
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.css'],
 })
 export class MovieListComponent implements OnInit {
-
-  constructor(private service: MovieService) { }
-
+  constructor(private service: MovieService) {}
+  @Input() i: number = 21;
+  popupIndex: number = 21;
+  @Input() movieGenres: any[] = [];
+  newGenres: any[] = [];
   @Input() singleMovie: Movie = {
-    title: "",
+    title: '',
     id: 0,
-    overview: "",
-    release_date: "",
+    overview: '',
+    release_date: '',
     vote_average: 0,
     genre_ids: [],
-    backdrop_path: "",
-    poster_path: "",
-    watchlist: false
+    backdrop_path: '',
+    poster_path: '',
+    watchlist: false,
+  };
+  get genreArray(): Genre[] {
+    return this.service.genreArray;
   }
-  @Output() isOnWatchListMovie = new EventEmitter<any>()
-  
+  @Output() isOnWatchListMovie = new EventEmitter<any>();
+
   ngOnInit(): void {
+    // this.getGenres();
+    console.log(this.genreArray);
+
+    this.genreArray.forEach((item) => {
+      this.movieGenres.forEach((id: any) => {
+        if (item.id === id) {
+          this.newGenres.push(item.name);
+        }
+      });
+    });
+    console.log(this.newGenres);
   }
 
   isWatchList() {
-    this.isOnWatchListMovie.emit()
+    this.isOnWatchListMovie.emit();
+  }
+  showPopup() {
+    this.popupIndex = this.i;
+  }
+  closePopup() {
+    this.popupIndex = 21;
   }
 }
